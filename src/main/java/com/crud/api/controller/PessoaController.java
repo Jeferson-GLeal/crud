10cbef524389cc3fd4256e01ed0ef14ac7170480
Jeferson <jeferson.gleal@outlook.com>
2024-11-11 20:02:48 -0300

    :sparkles:[feat] Implementado o endpoint pesquisar por nome

--- src/main/java/com/crud/api/controller/PessoaController.java
+++ src/main/java/com/crud/api/controller/PessoaController.java
@@ -5,6 +5,7 @@ import com.crud.api.domain.model.repository.PessoaRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,7 +18,11 @@ public class PessoaController {
 
     @GetMapping("/pessoas")
     public List<Pessoa> listar() {
-
         return pessoaRepository.findAll();
+    }
+
+    @GetMapping("/pessoas/{nome}")
+    public List<Pessoa> listarPorNome(@PathVariable String nome) {
+        return pessoaRepository.findByNomeContaining(nome);
     }
 }
--- src/main/java/com/crud/api/domain/model/repository/PessoaRepository.java
+++ src/main/java/com/crud/api/domain/model/repository/PessoaRepository.java
@@ -4,7 +4,10 @@ import com.crud.api.domain.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
+    List<Pessoa> findByNomeContaining(String nome);
 }
