755a9685647b3bc8fef17591b5feec980fbfd36b
Jeferson <jeferson.gleal@outlook.com>
2024-11-15 19:53:56 -0300

    :arrow_up:[update] Aprimoramente de mensagens de campos nulos ou vazios.

--- src/main/java/com/crud/api/domain/model/Pessoa.java
+++ src/main/java/com/crud/api/domain/model/Pessoa.java
@@ -2,10 +2,7 @@ package com.crud.api.domain.model;
 
 import com.crud.api.domain.validation.ValidationGroups;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -22,23 +19,27 @@ public class Pessoa {
     private Long id;
 
     @NotBlank
-    @Size(max = 60)
+    @Size(min = 5, max = 60)
     private String nome;
 
-    @NotBlank
-    @Size(max = 60)
+    @Size(min = 5, max = 60)
     @Column(name = "sobrenome")
     private String sobreNome;
 
+    @Size(min = 1, max = 2)
     private String idade;
+
+    @Pattern(regexp = "[0-9]{9}")
     private String rg;
+
+    @Pattern(regexp = "[0-9]{11}")
     private String cpf;
 
+    @Pattern(regexp = "[0-9]{8}")
     @Column(name = "datanasc")
     private String dataNascimento;
 
-    @NotBlank
-    @Size(max = 20)
+    @Pattern(regexp = "[0-9]{11}")
     private String telefone;
 
     @Email
@@ -47,6 +48,9 @@ public class Pessoa {
     @Column(name = "estadocivil")
     private String estadoCivil;
 
+    @Size(min = 5, max = 60)
     private String escolaridade;
+
+    @Size(min = 5, max = 60)
     private String profissao;
 }
